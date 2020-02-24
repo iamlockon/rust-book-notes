65313f7632bd9ce85ab114d5179e0e763957f4d9
iamlockon <xdddxyyyxzzz123@gmail.com>
2020-02-24 11:01:52 +0800

    add traits

--- traits/src/lib.rs
+++ traits/src/lib.rs
@@ -1,3 +1,5 @@
+use std::fmt::{ Display, Debug };
+
 // One can't implement traits on a type if none of the trait or type is local to the crate.
 // That is to say, we can't implement external traits on external types.
 // This 'orphan rule' is to avoid two crates implement the same trait for the same type.
@@ -17,7 +19,7 @@ pub struct NewsArticle {
 
 impl Summary for NewsArticle {
     fn summarize(&self) -> String {
-        format!("{}, by {} ({})", self.headline, self,author, self.location)
+        format!("{}, by {} ({})", self.headline, self.author, self.location)
     }
 }
 
@@ -35,11 +37,68 @@ impl Summary for Tweet {
 }
 
 // syntax sugar for trait bound, convenient
-pub fn notify(item: impl Summary) {
+pub fn notify(item: impl Summary + Display) -> () { // use `+` for multiple trait bounds
     println!("{}", item.summarize());
 }
 
 // trait bound syntax, more general
-pub fn notify_tb<T: Summary>(item1: T, item2: T) {
+pub fn notify_2<T: Summary + Display, U: Debug>(item1: T, item2: U) -> () {
     println!("blablabla");
 }
+// the above one is equivalent to:
+pub fn notify_3<T, U>(item1: T, item2: U) -> ()
+    where T: Summary + Display, // use `where` for shorter function signature
+          U: Debug
+{
+    ()
+}
+
+// This won't compile, `impl Summary` cannot return different concrete types
+// fn return_summarizable(switch: bool) -> impl Summary {
+//     if switch {
+//         NewsArticle {
+//             headline: String::from("Title"),
+//             location: String::from("PA"),
+//             author: String::from("Ice"),
+//             content: String::from("ccc"),
+//         }
+//     } else {
+//         Tweet {
+//             username: String::from("Icc"),
+//             content: String::from("cccc"),
+//             reply: false,
+//             retweet: false,
+//         }
+//     }
+// }
+
+// Conditionally implement methods for structs on a generic type depending on trait bounds
+struct Pair<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Pair<T> {
+    fn new(x:T, y:T) -> Self { // Pair always implement `new`
+        Self {
+            x,
+            y
+        }
+    }
+}
+
+impl<T: Display + PartialOrd> Pair<T> {
+    fn cmp_display(&self) { // Pair only implement cmp_display if T implements PartialOrd and Display
+        if self.x >= self.y {
+            println!("Largest member is x = {}", self.x);
+        } else {
+            println!("Largest member is y = {}", self.y);
+        }
+    }
+}
+// We can also do the same for trait implementations(ex: ToString)
+// impl<T: Display> ToString for T {
+//  
+// }
+// And that's why we can write:
+// let s = 3.to_string();
--- traits/src/main.rs
+++ traits/src/main.rs
@@ -1,3 +1,48 @@
+mod lib; // load lib.rs
+use crate::lib::Summary;
+
+fn largest_copy<T: PartialOrd + Copy>(list: &[T]) -> T {
+    let mut largest = list[0];
+
+    for &item in list.iter() { // item reference is destructured, making type of item T
+        if item > largest { // compare Ts
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+fn largest<T: PartialOrd>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+
+    for item in list.iter() { // type of item &T
+        if item > largest { // compare &T is possible since references to types implementing PartialOrd also implement PartialOrd.
+            largest = item;
+        }
+    }
+
+    largest
+}
+
 fn main() {
-    println!("Hello, world!");
+    let number_list = vec![34, 56, 23, 66, 77];
+
+    let result = largest(&number_list);
+    println!("Largest number {}", result);
+
+    let char_list = vec!['y', 'b', 'a', 'q'];
+    let result = largest_copy(&char_list);
+    println!("Largest char {}", result);
+    
+    let news = lib::NewsArticle {
+        headline: String::from("Title"),
+        location: String::from("PA"),
+        author: String::from("Ice"),
+        content: String::from("ccc"),
+    };
+
+    println!("{}", news.summarize());
+
+        
 }
