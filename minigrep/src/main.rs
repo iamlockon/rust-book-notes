896e7a68ebfc9f6e9abf5bd0c4fd3d0b13f5e716
iamlockon <xdddxyyyxzzz123@gmail.com>
2020-02-25 09:53:54 +0800

    add minigrep

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -3,15 +3,15 @@ use std::process;
 use minigrep::Config;
 
 fn main() {
-    let mut args: Vec<String> = env::args().collect();
+    let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| { // we care about return value
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config) { // we don't care about return value
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
