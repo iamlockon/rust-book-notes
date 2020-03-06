30a1776d850eeb0442118a9f04ebcfd2f813233b
iamlockon <xdddxyyyxzzz123@gmail.com>
2020-03-06 10:05:13 +0800

    edit minigrep

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -10,13 +10,25 @@ pub struct Config {
 
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments, need 3 at least");
-        }
+    // Old version
+    // pub fn new(args: &[String]) -> Result<Config, &'static str> {
+    pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
+        // if args.len() < 3 {
+        //     return Err("not enough arguments, need 3 at least");
+        // }
 
-        let query = args[1].clone();
-        let filename = args[2].clone();
+        // let query = args[1].clone();
+        // let filename = args[2].clone();
+
+        args.next();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Expected a query string, but get none."),
+        };
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Expected a file name, but get none."),
+        };
     
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
         Ok(Config { query, filename, case_sensitive })
@@ -67,15 +79,18 @@ mod tests {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
+    // Old version
+    // let mut results = Vec::new();
+    // for line in contents.lines() {
+    //     if line.contains(query) {
+    //         results.push(line);
+    //     }
+    // }
+    // results
 
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents.lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -3,9 +3,14 @@ use std::process;
 use minigrep::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::new(&args).unwrap_or_else(|err| { // we care about return value
+    // Old version
+    // let args: Vec<String> = env::args().collect();
+    // let config = Config::new(&args).unwrap_or_else(|err| { // we care about return value
+    //     eprintln!("Problem parsing arguments: {}", err);
+    //     process::exit(1);
+    // });
+    
+    let config = Config::new(env::args()).unwrap_or_else(|err| { // env::args return an iterator. 
         eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
